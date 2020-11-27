9ef592ee2c77989be67c1633c77ebe68a26b98bb
xyz600 <xyz600600@gmail.com>
2020-11-27 22:51:03 +0900

    add impl to output prob / sol json result

---
+++ src/main.cpp
@@ -0,0 +1,353 @@
+#include <algorithm>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+
+#include <boost/algorithm/string.hpp>
+#include <nlohmann/json.hpp>
+
+struct Operation
+{
+    std::size_t operation_id;
+    std::size_t process_time;
+    std::size_t machine_id;
+    std::size_t job_order;
+    std::size_t job_id;
+
+    nlohmann::json to_json() const
+    {
+        auto ret = nlohmann::json::object();
+        ret["id"] = operation_id;
+        ret["time"] = process_time;
+        ret["machine"] = machine_id;
+        ret["job"] = job_id;
+        return ret;
+    }
+};
+
+std::ostream &operator<<(std::ostream &out, const Operation &op)
+{
+    out << "(id, time, machine) = (" << op.operation_id << ", " << op.process_time << ", " << op.machine_id << ")";
+    return out;
+}
+
+class Problem
+{
+public:
+    std::size_t machine_size() const { return machine_size_; }
+    std::size_t job_size() const { return job_size_; }
+
+    std::size_t operation_size() const { return operation_list_.size(); }
+    std::size_t job_length(const std::size_t job_id) const { return operation_table_[job_id].size(); }
+    Operation &operation(std::size_t job_id, std::size_t order)
+    {
+        return operation_list_[operation_table_[job_id][order]];
+    }
+    const Operation &operation(std::size_t job_id, std::size_t order) const
+    {
+        return operation_list_[operation_table_[job_id][order]];
+    }
+    const Operation &operation(std::size_t operation_id) const { return operation_list_[operation_id]; }
+    Operation &operation(std::size_t operation_id) { return operation_list_[operation_id]; }
+
+    Problem(const std::string &filepath) { load(filepath); }
+
+    void to_json(const std::string &filepath)
+    {
+        using json = nlohmann::json;
+        json data;
+        data["machine_size"] = machine_size();
+        data["job_size"] = job_size();
+        data["operation_table"] = {};
+        for (auto &row : operation_table_)
+        {
+            auto ary = json::array();
+            for (auto &elem : row)
+            {
+                ary.push_back(elem);
+            }
+            data["operation_table"].push_back(ary);
+        }
+        data["operations"] = {};
+        for (auto &op : operation_list_)
+        {
+            data["operations"].push_back(op.to_json());
+        }
+
+        std::ofstream fout(filepath);
+        if (!fout.is_open())
+        {
+            throw std::runtime_error("file cannot open @ Problem::to_json");
+        }
+        fout << data.dump(2) << std::endl;
+    }
+
+private:
+    std::size_t machine_size_;
+    std::size_t job_size_;
+    std::vector<std::vector<std::size_t>> operation_table_;
+    std::vector<Operation> operation_list_;
+
+    void load(const std::string &filepath)
+    {
+        std::ifstream fin(filepath);
+        if (!fin.is_open())
+        {
+            throw std::runtime_error("file not found @ Problem::load");
+        }
+        std::string _;
+        {
+            // skip header
+            std::getline(fin, _);
+            std::size_t time_seed, machine_seed, upper_bound, lower_bound;
+            fin >> job_size_ >> machine_size_ >> time_seed >> machine_seed >> upper_bound >> lower_bound;
+            // skip \n
+            std::getline(fin, _);
+        }
+        operation_table_.resize(job_size_);
+
+        // skip header
+        std::getline(fin, _);
+        // times
+        {
+            std::size_t id = 0;
+            for (std::size_t job_id = 0; job_id < job_size_; job_id++)
+            {
+                auto &row = operation_table_[job_id];
+                std::string line;
+                std::getline(fin, line);
+
+                boost::trim(line);
+                std::vector<std::string> token;
+                boost::split(token, line, boost::is_space(), boost::algorithm::token_compress_on);
+                row.resize(token.size());
+
+                for (std::size_t i = 0; i < token.size(); i++)
+                {
+                    row[i] = id++;
+                    Operation op;
+                    op.process_time = std::stol(token[i]);
+                    op.operation_id = row[i];
+                    op.job_id = job_id;
+                    op.job_order = i;
+                    operation_list_.push_back(op);
+                }
+            }
+        }
+        // skip header
+        std::getline(fin, _);
+        // machine
+        {
+            std::size_t operation_id = 0;
+            for (auto &op : operation_list_)
+            {
+                fin >> op.machine_id;
+            }
+        }
+    }
+};
+
+class Solution
+{
+public:
+    struct State
+    {
+        std::size_t operation_id;
+        std::size_t start_time;
+
+        State(std::size_t operation_id, std::size_t start_time) : operation_id(operation_id), start_time(start_time) {}
+        State() {}
+        State(const State &src) : operation_id(src.operation_id), start_time(src.start_time) {}
+    };
+
+    Solution(const Problem &prob) { process_list_.resize(prob.machine_size() + 1); }
+
+    std::vector<State> &process_list(const std::size_t machine_id) { return process_list_[machine_id]; }
+    const std::vector<State> &process_list(const std::size_t machine_id) const { return process_list_[machine_id]; }
+
+    void validate(const Problem &prob)
+    {
+        std::vector<bool> used(prob.operation_size(), false);
+        for (auto &row : process_list_)
+        {
+            for (auto &elem : row)
+            {
+                assert(elem.operation_id < prob.job_size() * prob.machine_size());
+                used[elem.operation_id] = true;
+            }
+        }
+        assert(std::count(used.begin(), used.end(), true) == prob.job_size() * prob.machine_size());
+    }
+
+    void to_json(const std::string &filepath)
+    {
+        using json = nlohmann::json;
+        json data;
+        data["process_list"] = {};
+        for (std::size_t i = 1; i < process_list_.size(); i++)
+        {
+            auto &row = process_list_[i];
+            auto ary = json::array();
+            for (auto &elem : row)
+            {
+                auto obj = json::object();
+                obj["start_time"] = elem.start_time;
+                obj["operation_id"] = elem.operation_id;
+                ary.push_back(obj);
+            }
+            data["process_list"].push_back(ary);
+        }
+        std::ofstream fout(filepath);
+        if (!fout.is_open())
+        {
+            throw std::runtime_error("file cannot open @ Solution::to_json");
+        }
+        fout << data.dump(2) << std::endl;
+    }
+
+private:
+    std::vector<std::vector<State>> process_list_;
+};
+
+class Solver
+{
+public:
+    Solution solve(const Problem &prob)
+    {
+        Solution sol(prob);
+        for (std::size_t job_id = 0; job_id < prob.job_size(); job_id++)
+        {
+            for (std::size_t order = 0; order < prob.job_length(job_id); order++)
+            {
+                const auto &op = prob.operation(job_id, order);
+                sol.process_list(op.machine_id).emplace_back(op.operation_id, 0);
+            }
+        }
+        return sol;
+    }
+
+    void setup_start_time(Solution &sol, const Problem &prob)
+    {
+        std::vector<std::vector<std::size_t>> graph(prob.operation_size());
+        std::vector<std::size_t> counter(prob.operation_size(), 0);
+
+        // グラフの構築 job の依存関係
+        for (std::size_t job_id = 0; job_id < prob.job_size(); job_id++)
+        {
+            for (std::size_t order = 0; order + 1 < prob.job_length(job_id); order++)
+            {
+                const auto cur_id = prob.operation(job_id, order).operation_id;
+                const auto next_id = prob.operation(job_id, order + 1).operation_id;
+                graph[cur_id].push_back(next_id);
+                counter[next_id]++;
+            }
+        }
+        // machine 処理順序の依存関係
+        for (std::size_t machine_id = 1; machine_id <= prob.machine_size(); machine_id++)
+        {
+            for (std::size_t order = 0; order + 1 < sol.process_list(machine_id).size(); order++)
+            {
+                const auto cur_id = sol.process_list(machine_id)[order].operation_id;
+                const auto next_id = sol.process_list(machine_id)[order + 1].operation_id;
+                graph[cur_id].push_back(next_id);
+                counter[next_id]++;
+            }
+        }
+
+        // operation_id が sol のどこにあるかを構築
+        std::vector<std::pair<std::size_t, std::size_t>> sol_place(prob.operation_size());
+        for (std::size_t machine_id = 1; machine_id <= prob.machine_size(); machine_id++)
+        {
+            const auto &list = sol.process_list(machine_id);
+            for (std::size_t order = 0; order < list.size(); order++)
+            {
+                const auto operation_id = sol.process_list(machine_id)[order].operation_id;
+                sol_place[operation_id].first = machine_id;
+                sol_place[operation_id].second = order;
+            }
+        }
+
+        // 初期化
+        for (std::size_t machine_id = 1; machine_id <= prob.machine_size(); machine_id++)
+        {
+            for (auto &elem : sol.process_list(machine_id))
+            {
+                elem.start_time = 0;
+            }
+        }
+
+        // topological sort order
+        std::queue<std::size_t> que;
+        for (std::size_t operation_id = 0; operation_id < prob.operation_size(); operation_id++)
+        {
+            if (counter[operation_id] == 0)
+            {
+                que.push(operation_id);
+            }
+        }
+
+        while (!que.empty())
+        {
+            const auto operation_id = que.front();
+            que.pop();
+
+            const auto machine_id = sol_place[operation_id].first;
+            const auto order_in_machine = sol_place[operation_id].second;
+            const auto cur_time = sol.process_list(machine_id)[order_in_machine].start_time;
+            // 配る DP
+            if (order_in_machine + 1 < sol.process_list(machine_id).size())
+            {
+                const auto next_start = cur_time + prob.operation(operation_id).process_time;
+                sol.process_list(machine_id)[order_in_machine + 1].start_time =
+                    std::max(sol.process_list(machine_id)[order_in_machine + 1].start_time, next_start);
+            }
+
+            const auto job_id = prob.operation(operation_id).job_id;
+            const auto job_order = prob.operation(operation_id).job_order;
+            if (job_order + 1 < prob.job_length(job_id))
+            {
+                const auto next_start = cur_time + prob.operation(operation_id).process_time;
+
+                const auto &next_op = prob.operation(job_id, job_order + 1);
+                const auto next_order_in_machine = sol_place[next_op.operation_id].second;
+                sol.process_list(next_op.machine_id)[next_order_in_machine].start_time =
+                    std::max(sol.process_list(next_op.machine_id)[next_order_in_machine].start_time, next_start);
+            }
+
+            for (const auto neighbor : graph[operation_id])
+            {
+                counter[neighbor]--;
+                if (counter[neighbor] == 0)
+                {
+                    que.push(neighbor);
+                }
+            }
+        }
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    const std::string filepath(argv[1]);
+    Problem problem(filepath);
+
+    std::cout << problem.job_size() << " " << problem.machine_size() << std::endl;
+    std::cout << problem.operation(0, 0) << std::endl;
+
+    Solver solver;
+    auto sol = solver.solve(problem);
+    solver.setup_start_time(sol, problem);
+    sol.validate(problem);
+
+    problem.to_json("problem.json");
+    sol.to_json("answer.json");
+
+    return 0;
+}
