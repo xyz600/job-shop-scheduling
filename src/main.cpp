f42f77174d3d4bf1037ce2e52ad8cd38220d9b71
xyz600 <xyz600600@gmail.com>
2020-12-19 12:04:49 +0900

    add GT-Algorithm implementation

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <iostream>
 #include <queue>
+#include <random>
 #include <set>
 #include <sstream>
 #include <stdexcept>
@@ -167,12 +168,50 @@ public:
         State(const State &src) : operation_id(src.operation_id), start_time(src.start_time) {}
     };
 
-    Solution(const Problem &prob) { process_list_.resize(prob.machine_size() + 1); }
+    Solution(const Problem &prob)
+    {
+        process_list_.resize(prob.machine_size() + 1);
+        order_in_machine_.resize(prob.operation_size());
+        std::fill(order_in_machine_.begin(), order_in_machine_.end(), std::numeric_limits<std::size_t>::max());
+    }
 
-    std::vector<State> &process_list(const std::size_t machine_id) { return process_list_[machine_id]; }
-    const std::vector<State> &process_list(const std::size_t machine_id) const { return process_list_[machine_id]; }
+    /**
+     * @brief operation_id の直前の operation が登録されている前提で、machine および job
+     * の制約を鑑みて最速で実行できる時間を計算
+     */
+    std::size_t fastest_start_time(const Problem &prob, const std::size_t operation_id) const
+    {
+        const auto machine_id = prob.operation(operation_id).machine_id;
+        std::size_t time_by_machine = 0;
+        if (!process_list_[machine_id].empty())
+        {
+            // current job の登録はまだ解になされていないため、push_back される前提
+            const auto target_order = process_list_[machine_id].size() - 1;
+            const auto target_operation_id = process_list_[machine_id][target_order].operation_id;
+            time_by_machine =
+                process_list_[machine_id][target_order].start_time + prob.operation(target_operation_id).process_time;
+        }
+        std::size_t time_by_job = 0;
+        if (prob.operation(operation_id).job_order > 0)
+        {
+            const auto job_id = prob.operation(operation_id).job_id;
+            const auto job_order = prob.operation(operation_id).job_order - 1;
+            const auto &prev_op = prob.operation(job_id, job_order);
+            time_by_job = process_list_[prev_op.machine_id][order_in_machine_[prev_op.operation_id]].start_time +
+                          prev_op.process_time;
+        }
+        return std::max(time_by_machine, time_by_job);
+    }
 
-    void validate(const Problem &prob)
+    void push_back_operation(const Problem &prob, const std::size_t operation_id)
+    {
+        const auto machine_id = prob.operation(operation_id).machine_id;
+        order_in_machine_[operation_id] = process_list_[machine_id].size();
+        // TODO: 修正
+        process_list_[machine_id].emplace_back(operation_id, fastest_start_time(prob, operation_id));
+    }
+
+    void validate(const Problem &prob) const
     {
         std::vector<bool> used(prob.operation_size(), false);
         for (auto &row : process_list_)
@@ -184,6 +223,8 @@ public:
             }
         }
         assert(std::count(used.begin(), used.end(), true) == prob.job_size() * prob.machine_size());
+
+        // TODO: order_in_machine_ と process_list_ の整合性を確かめる
     }
 
     void to_json(const std::string &filepath)
@@ -212,28 +253,7 @@ public:
         fout << data.dump(2) << std::endl;
     }
 
-private:
-    std::vector<std::vector<State>> process_list_;
-};
-
-class Solver
-{
-public:
-    Solution solve(const Problem &prob)
-    {
-        Solution sol(prob);
-        for (std::size_t job_id = 0; job_id < prob.job_size(); job_id++)
-        {
-            for (std::size_t order = 0; order < prob.job_length(job_id); order++)
-            {
-                const auto &op = prob.operation(job_id, order);
-                sol.process_list(op.machine_id).emplace_back(op.operation_id, 0);
-            }
-        }
-        return sol;
-    }
-
-    void setup_start_time(Solution &sol, const Problem &prob)
+    void register_start_time(const Problem &prob)
     {
         std::vector<std::vector<std::size_t>> graph(prob.operation_size());
         std::vector<std::size_t> counter(prob.operation_size(), 0);
@@ -249,13 +269,14 @@ public:
                 counter[next_id]++;
             }
         }
+
         // machine 処理順序の依存関係
         for (std::size_t machine_id = 1; machine_id <= prob.machine_size(); machine_id++)
         {
-            for (std::size_t order = 0; order + 1 < sol.process_list(machine_id).size(); order++)
+            for (std::size_t order = 0; order + 1 < process_list_[machine_id].size(); order++)
             {
-                const auto cur_id = sol.process_list(machine_id)[order].operation_id;
-                const auto next_id = sol.process_list(machine_id)[order + 1].operation_id;
+                const auto cur_id = process_list_[machine_id][order].operation_id;
+                const auto next_id = process_list_[machine_id][order + 1].operation_id;
                 graph[cur_id].push_back(next_id);
                 counter[next_id]++;
             }
@@ -265,10 +286,10 @@ public:
         std::vector<std::pair<std::size_t, std::size_t>> sol_place(prob.operation_size());
         for (std::size_t machine_id = 1; machine_id <= prob.machine_size(); machine_id++)
         {
-            const auto &list = sol.process_list(machine_id);
+            const auto &list = process_list_[machine_id];
             for (std::size_t order = 0; order < list.size(); order++)
             {
-                const auto operation_id = sol.process_list(machine_id)[order].operation_id;
+                const auto operation_id = process_list_[machine_id][order].operation_id;
                 sol_place[operation_id].first = machine_id;
                 sol_place[operation_id].second = order;
             }
@@ -277,7 +298,7 @@ public:
         // 初期化
         for (std::size_t machine_id = 1; machine_id <= prob.machine_size(); machine_id++)
         {
-            for (auto &elem : sol.process_list(machine_id))
+            for (auto &elem : process_list_[machine_id])
             {
                 elem.start_time = 0;
             }
@@ -300,13 +321,13 @@ public:
 
             const auto machine_id = sol_place[operation_id].first;
             const auto order_in_machine = sol_place[operation_id].second;
-            const auto cur_time = sol.process_list(machine_id)[order_in_machine].start_time;
+            const auto cur_time = process_list_[machine_id][order_in_machine].start_time;
             // 配る DP
-            if (order_in_machine + 1 < sol.process_list(machine_id).size())
+            if (order_in_machine + 1 < process_list_[machine_id].size())
             {
                 const auto next_start = cur_time + prob.operation(operation_id).process_time;
-                sol.process_list(machine_id)[order_in_machine + 1].start_time =
-                    std::max(sol.process_list(machine_id)[order_in_machine + 1].start_time, next_start);
+                process_list_[machine_id][order_in_machine + 1].start_time =
+                    std::max(process_list_[machine_id][order_in_machine + 1].start_time, next_start);
             }
 
             const auto job_id = prob.operation(operation_id).job_id;
@@ -317,8 +338,8 @@ public:
 
                 const auto &next_op = prob.operation(job_id, job_order + 1);
                 const auto next_order_in_machine = sol_place[next_op.operation_id].second;
-                sol.process_list(next_op.machine_id)[next_order_in_machine].start_time =
-                    std::max(sol.process_list(next_op.machine_id)[next_order_in_machine].start_time, next_start);
+                process_list_[next_op.machine_id][next_order_in_machine].start_time =
+                    std::max(process_list_[next_op.machine_id][next_order_in_machine].start_time, next_start);
             }
 
             for (const auto neighbor : graph[operation_id])
@@ -331,6 +352,83 @@ public:
             }
         }
     }
+
+private:
+    std::vector<std::vector<State>> process_list_;
+    // operation id -> その operation が利用する機械で何番目に処理されるか
+    std::vector<std::size_t> order_in_machine_;
+};
+
+class Solver
+{
+public:
+    Solution solve(const Problem &prob)
+    {
+        Solution sol(prob);
+
+        // 各 job の何番目の operation をこなしているか
+        std::vector<std::size_t> operation_set(prob.job_size() + 1, 0);
+
+        auto front_operation = [&](std::size_t job_id) { return prob.operation(job_id, operation_set[job_id]); };
+
+        // 完了した job の個数
+        std::size_t finish_counter = 0;
+
+        while (finish_counter < prob.job_size())
+        {
+            // 終了時間が最短の命令を選択する
+            std::size_t fastest_job_id = std::numeric_limits<std::size_t>::max();
+            std::size_t fastest_finish_time = std::numeric_limits<std::size_t>::max();
+            for (std::size_t job_id = 0; job_id < prob.job_size(); job_id++)
+            {
+                if (operation_set[job_id] < prob.job_length(job_id))
+                {
+                    const auto &operation = front_operation(job_id);
+                    const std::size_t operation_id = operation.operation_id;
+
+                    const auto finish_time = sol.fastest_start_time(prob, operation_id) + operation.process_time;
+                    if (fastest_finish_time > finish_time)
+                    {
+                        fastest_finish_time = finish_time;
+                        fastest_job_id = job_id;
+                    }
+                }
+            }
+
+            const auto &fastest_operation = front_operation(fastest_job_id);
+
+            // random pick-up する operation の候補
+            std::vector<std::size_t> conflict_set;
+            for (std::size_t job_id = 0; job_id < prob.job_size(); job_id++)
+            {
+                if (operation_set[job_id] < prob.job_length(job_id))
+                {
+                    const auto &selected_operation = front_operation(job_id);
+                    if (fastest_operation.machine_id == selected_operation.machine_id &&
+                        sol.fastest_start_time(prob, selected_operation.operation_id) < fastest_finish_time)
+                    {
+                        conflict_set.push_back(job_id);
+                    }
+                }
+            }
+
+            // ランダムに1つ選択
+            std::uniform_int_distribution<> rand(0, conflict_set.size() - 1);
+            const auto selected_job_id = conflict_set[rand(mt)];
+            sol.push_back_operation(prob, front_operation(selected_job_id).operation_id);
+
+            // 同一 job を次に進める
+            operation_set[selected_job_id]++;
+            if (operation_set[selected_job_id] == prob.job_length(selected_job_id))
+            {
+                finish_counter++;
+            }
+        }
+        return sol;
+    }
+
+private:
+    std::mt19937_64 mt;
 };
 
 int main(int argc, char *argv[])
@@ -343,7 +441,6 @@ int main(int argc, char *argv[])
 
     Solver solver;
     auto sol = solver.solve(problem);
-    solver.setup_start_time(sol, problem);
     sol.validate(problem);
 
     problem.to_json("problem.json");
